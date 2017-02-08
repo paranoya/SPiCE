dbe9d240dc04974a9b795b47420a7f0b1b8c05f8
Delosari <Delosari@users.noreply.github.com>
2017-02-08 11:20:59 +0100

    adding numpy
    adding some maths

--- Testing/example/hello_world.py
+++ Testing/example/hello_world.py
@@ -2,12 +2,16 @@
 Created on Feb 8, 2017
 
 @author: vital
-'''
-print 'hello world'
+import numpy as np
 
+'''
 print 'How are you doing?'
 
 print 'Vamos git!!'
 
 print 'stranger things = movidas chungas'
+
+a = np.array([1,2,3])
+
+print a
 
